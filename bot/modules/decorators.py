e5fd3142d8134e0d054101ad4f420bc96d21bfdb
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2024-12-11 08:06:59 +0530

    Create decorators.py

---
+++ bot/modules/decorators.py
@@ -0,0 +1,21 @@
+from telethon.events import NewMessage, CallbackQuery
+from typing import Callable
+from functools import wraps
+from bot.config import Telegram
+
+def verify_user(private: bool = False):
+    
+    def decorator(func: Callable):
+        @wraps(func)
+        async def wrapper(update: NewMessage.Event | CallbackQuery.Event):
+            if private and not update.is_private:
+                return
+
+            chat_id = str(update.chat_id)
+
+            if not Telegram.ALLOWED_USER_IDS or chat_id in Telegram.ALLOWED_USER_IDS:
+                return await func(update)
+
+        return wrapper
+    return decorator
+  
